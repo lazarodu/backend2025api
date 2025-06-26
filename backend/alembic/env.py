e3c4e7da927ad87175f5ac33bae2694b6a8469d7
lazarodu <lazarodu@gmail.com>
2025-06-26 00:11:27 -0300

    testes

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,33 +1,30 @@
-from alembic import context
-from sqlalchemy.ext.asyncio import create_async_engine
+import os
 from logging.config import fileConfig
 
-from blog.infra.database import Base
-from blog.infra.settings import DATABASE_URL
+from sqlalchemy import engine_from_config, pool
+from alembic import context
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Load .env
+load_dotenv(dotenv_path=Path(".env"))
+
+# Alembic Config
+config = context.config
+fileConfig(config.config_file_name)
+
+# Use env var for DB URL
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL is None:
+    raise Exception("DATABASE_URL not found in .env")
+
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+# Target metadata
+from blog.infra.database import Base 
 from blog.infra.models.user_model import UserModel
 from blog.infra.models.post_model import PostModel
 from blog.infra.models.comment_model import CommentModel
 
-config = context.config
-fileConfig(config.config_file_name)
+
 target_metadata = Base.metadata
-print("DATABASE_URL:", DATABASE_URL)
-def run_migrations_online():
-    connectable = create_async_engine(DATABASE_URL, echo=True)
-
-    async def do_migrations():
-        async with connectable.begin() as connection:
-            await connection.run_sync(
-                lambda conn: context.configure(
-                    connection=conn,
-                    target_metadata=target_metadata,
-                    literal_binds=True,
-                    render_as_batch=True,
-                )
-            )
-            context.run_migrations()
-
-    import asyncio
-    asyncio.run(do_migrations())
-
-run_migrations_online()
